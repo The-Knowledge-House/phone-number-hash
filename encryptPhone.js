aabbff8db1bebca45d564a4b06c23357a16eaa18
Jennifer Smith <59855990+JenniferSmith007@users.noreply.github.com>
2020-10-10 18:59:16 -0400

    encryptPhoneNumber

--- encryptPhone.js
+++ encryptPhone.js
@@ -4,5 +4,20 @@
  * ' 111-223-1123 ' should be ' ***-***-1123 '
  */
 
+function encryptPhoneNumber(number) {
+  let firstNumber = number.split('');
+  let secondNumber = [];
+  for (let x = 0; x < firstNumber.length; x++) {
+    if (firstNumber[x] == `-` || x >= 8) {
+    secondNumber.push(firstNumber[x]);
+    } else {
+      secondNumber.push(firstNumber[x] = `*`);
+    }
+  }
+  return secondNumber.join(``);
+}
+encryptPhoneNumber(`646-597-2606`);
+
+
 
 module.exports = encryptPhoneNumber;
