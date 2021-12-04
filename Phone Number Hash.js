43cb8620cd9c0da8ee6ac1dfc20b2b5a6998ffc1
Khazinne <86040679+Khazinne@users.noreply.github.com>
2021-12-03 22:21:02 -0500

    Update Phone Number Hash.js

--- Phone Number Hash.js
+++ Phone Number Hash.js
@@ -1,17 +1,13 @@
-//problem 
-const encryptPhoneNumber = require('../encryptPhone')
+/**
+ * @function encryptPhoneNumber should take a phone number and hash it for security and privacy purposes.
+ * @param number will always be a string formatted as such '111-223-1123'. Your function should return the first 6 digits as * and only the last 4 shown. Ex:
+ * ' 111-223-1123 ' should be ' ***-***-1123 '
+ */
 
-describe('Encrypy Phone Number', () => {
-  describe.each([['888-445-3421', '***-***-3421']])(
-    'double(%d)',
-    (input, expected) => {
-      test(`returns hashed number: ${expected}`, () => {
-        expect(encryptPhoneNumber(input)).toBe(expected)
-      })
-    }
-  )
-})
 
+ module.exports = encryptPhoneNumber;
+
+//this needs to go in the encryptphone.js and will then work
 //solution
 function encryptPhoneNumber(num) {
     let encrypted = "";
